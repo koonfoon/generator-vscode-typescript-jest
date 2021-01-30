0a4aa8e3d61c0854c161fe611aca4df4cc5daa39
koonfoon <koonfoon@gmail.com>
2021-01-30 22:09:46 +0800

    add template files

---
+++ generators/app/templates/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        //"eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
