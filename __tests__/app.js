740f68fd3666f1c2f90ea1eb7a8239f53ce5fbed
koonfoon <koonfoon@gmail.com>
2023-08-16 00:06:44 +0800

    newer version

--- __tests__/app.js
+++ __tests__/app.js
@@ -5,24 +5,12 @@ const helpers = require("yeoman-test");
 
 describe("generator-vscode-typescript-jest:app", () => {
   beforeAll(() => {
-    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
-      someAnswer: true,
-      appName: "App test name",
-      appStartVersion: "1.0.1",
-      appDescription: "App test description",
-      license: "MIT",
-    });
+    return helpers
+      .run(path.join(__dirname, "../generators/app"))
+      .withPrompts({ someAnswer: true });
   });
 
   it("creates files", () => {
-    assert.file([
-      ".eslintrc.json",
-      ".prettierignore",
-      ".prettierrc.json",
-      "jest.config.js",
-      "package.json",
-      "tsconfig.json",
-      "tsc-multi.json",
-    ]);
+    assert.file(["dummyfile.txt"]);
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,108 +1,40 @@
-// Some tesitng comment
 "use strict";
 const Generator = require("yeoman-generator");
 const chalk = require("chalk");
 const yosay = require("yosay");
-const fse = require("fs-extra");
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
       yosay(
-        `Welcome to the praiseworthy ${chalk.red(
+        `Welcome to the lovely ${chalk.red(
           "generator-vscode-typescript-jest"
         )} generator!`
       )
     );
 
     const prompts = [
-      // {
-      //   type: "confirm",
-      //   name: "someAnswer",
-      //   message: "Would you like to enable this option?",
-      //   default: true,
-      // },
-      // Ask user for project name
       {
-        type: "input",
-        name: "appName",
-        message: "Your project name",
-        default: this.appname,
-      },
-      {
-        type: "input",
-        name: "appStartVersion",
-        message: "Project version",
-        default: "0.0.0",
-      },
-      {
-        type: "input",
-        name: "appDescription",
-        message: "Project description:",
-      },
-      {
-        type: "input",
-        name: "appAuthorName",
-        message: "Your name:",
-        default: this.user.git.name,
-      },
+        type: "confirm",
+        name: "someAnswer",
+        message: "Would you like to enable this option?",
+        default: true
+      }
     ];
 
-    return this.prompt(prompts).then((props) => {
+    return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props;
-      this.composeWith(require.resolve("generator-license"), {
-        name: this.props.appAuthorName,
-      });
     });
   }
 
   writing() {
-    // Copying all config files to project root directory
     this.fs.copy(
-      this.templatePath(".eslintrc.json.temp"),
-      this.destinationPath(".eslintrc.json")
+      this.templatePath("dummyfile.txt"),
+      this.destinationPath("dummyfile.txt")
     );
-    this.fs.copy(
-      this.templatePath(".prettierignoreTemp"),
-      this.destinationPath(".prettierignore")
-    );
-    this.fs.copy(
-      this.templatePath(".prettierrc.json.temp"),
-      this.destinationPath(".prettierrc.json")
-    );
-    this.fs.copy(
-      this.templatePath("jest.config.js.temp"),
-      this.destinationPath("jest.config.js")
-    );
-    this.fs.copyTpl(
-      this.templatePath("package.json.temp"),
-      this.destinationPath("package.json"),
-      {
-        packageAppName: this.props.appName,
-        packageAppStartVersion: this.props.appStartVersion,
-        packageAppDescription: this.props.appDescription,
-        packageAppAuthor: this.props.appAuthorName,
-      }
-    );
-    this.fs.copy(
-      this.templatePath("tsconfig.json.temp"),
-      this.destinationPath("tsconfig.json")
-    );
-    this.fs.copy(
-      this.templatePath("tsc-multi.json.temp"),
-      this.destinationPath("tsc-multi.json")
-    );
-    // Creating project directory
-    fse.ensureDir("./src");
-    fse.ensureDir("./build");
-    fse.ensureDir("./__tests__");
   }
 
-  install() {
-    this.installDependencies({
-      bower: false,
-    });
-  }
+  install() {}
 };
