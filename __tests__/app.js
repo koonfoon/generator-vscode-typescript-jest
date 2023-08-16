fb32057c4dfb6930a23db53a752559b6f3e52052
koonfoon <koonfoon@gmail.com>
2023-08-16 15:12:08 +0800

    test: :test_tube: update test codes

--- __tests__/app.js
+++ __tests__/app.js
@@ -5,12 +5,22 @@ const helpers = require("yeoman-test");
 
 describe("generator-vscode-typescript-jest:app", () => {
   beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, "../generators/app"))
-      .withPrompts({ someAnswer: true });
+    return helpers.run(path.join(__dirname, "../generators/app")).withPrompts({
+      someAnswer: true,
+      appName: "App test name",
+      appStartVersion: "1.0.1",
+      appDescription: "App test description",
+      license: "MIT"
+    });
   });
 
   it("creates files", () => {
-    assert.file(["dummyfile.txt"]);
+    assert.file([
+      ".eslintrc.json",
+      ".prettierrc.json",
+      "jest.config.js",
+      "package.json",
+      "tsconfig.json"
+    ]);
   });
 });
